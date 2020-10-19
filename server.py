7d15e231c78be973343b11118d1426779657dfa9
shashank <aitalshashank2@gmail.com>
2020-10-19 20:05:21 +0530

    Make requests to the master

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ import json
 with io.open('config.yml', 'r') as stream:
     data_loaded = yaml.safe_load(stream)
 
-destination_file = data_loaded["destination_file"]
+destination_file = data_loaded["DESTINATION_FILE"]
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 def server():
     payload = request.get_json()
-    authorized_keys = json.loads(payload)["authorized_keys"]
+    authorized_keys = payload["authorized_keys"]
 
     f = open(destination_file, 'w')
     f.write(authorized_keys)
--- track.py
+++ track.py
@@ -2,42 +2,61 @@ import os
 import io
 import subprocess
 import yaml
+import json
+import requests
+
+with io.open('config.yml', 'r') as stream:
+	try:
+		CONFIG_VARS = yaml.safe_load(stream)
+	except yaml.YAMLError as err:
+		print(err)
+		raise
+
+MASTER_URL = CONFIG_VARS['MASTER']['URL']
+MASTER_ENDPOINTS = CONFIG_VARS['MASTER']['ENDPOINTS']
 
 def login(log):
 	l = log.split()
 	print("LOGIN")
-	month = l[0]
-	date = l[1]
-	time = l[2]
-	device = l[3]
-	user = l[8]
-	ip = l[10]
-	port = l[12]
 
-	print(month, date, time, device, user, ip, port)
+	payload = {
+		"month": l[0],
+		"date": l[1],
+		"time": l[2],
+		"host": l[3],
+		"user": l[8],
+		"ip": l[10],
+		"port": l[12]
+	}
+
+	# payload = json.dumps(payload)
+	url = MASTER_URL + MASTER_ENDPOINTS['LOGIN']
+
+	f = requests.post(url, json=payload)
+
 
 def logout(log):
 	l = log.split()
 	print("LOGOUT")
-	month = l[0]
-	date = l[1]
-	time = l[2]
-	device = l[3]
-	user = l[8]
-	ip = l[9]
-	port = l[11]
 
-	print(month, date, time, device, user, ip, port)
+	payload = {
+		"month": l[0],
+		"date": l[1],
+		"time": l[2],
+		"host": l[3],
+		"user": l[8],
+		"ip": l[9],
+		"port": l[11]
+	}
+
+	# payload = json.dumps(payload)
+	url = MASTER_URL + MASTER_ENDPOINTS['LOGOUT']
+
+	f = requests.post(url, json=payload)
 
 def track():
-	with io.open('config.yml', 'r') as stream:
-		try:
-			CONFIG_VARS = yaml.safe_load(stream)
-		except yaml.YAMLError as err:
-			print(err)
-			raise FileNotFoundError
 	
-	auth_log = CONFIG_VARS['auth_log']
+	auth_log = CONFIG_VARS['AUTH_LOG']
 
 	f = subprocess.Popen(['tail', '-F', auth_log], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 	

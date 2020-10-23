72bf66dbe59797af0237d1fb0ee868d8f0b83283
shashank <aitalshashank2@gmail.com>
2020-10-23 16:15:08 +0530

    Logging the remote client too

--- track.py
+++ track.py
@@ -14,13 +14,31 @@ with io.open('config.yml', 'r') as stream:
 		print(err)
 		raise
 
+AUTH_LOG = CONFIG_VARS['AUTH_LOG']
 MASTER_URL = CONFIG_VARS['MASTER']['URL']
-MASTER_ENDPOINTS = CONFIG_VARS['MASTER']['ENDPOINTS']
+ENDPOINT_LOG = CONFIG_VARS['MASTER']['ENDPOINT_LOG']
 MASTER_SECRET = CONFIG_VARS['MASTER']['SECRET']
+AUTHORIZED_KEYS = CONFIG_VARS['DESTINATION_FILE']
 
 def login(log):
 	l = log.split()
 	print("LOGIN")
+
+	RSAKey = l[-1]
+	encodedKeys = subprocess.run(['ssh-keygen', '-lf', AUTHORIZED_KEYS], capture_output=True, text=True).stdout.splitlines()
+	
+	client = None
+	activity = "LOGIN"
+
+	if l[-2] == "RSA":
+		activity = "LOGIN_SSH_KEY"
+		for x in encodedKeys:
+			x = x.split()
+			if x[1] == RSAKey:
+				client = x[2]
+				break
+	else:
+		activity = "LOGIN_SSH_PWD"
 
 	payload = {
 		"month": l[0],
@@ -29,7 +47,9 @@ def login(log):
 		"host": l[3],
 		"user": l[8],
 		"ip": l[10],
-		"port": l[12]
+		"port": l[12],
+		"activity": activity,
+		"client": client
 	}
 
 	signature  = 'sha1=' + hmac.new(MASTER_SECRET.encode(), json.dumps(payload).encode(), hashlib.sha1).hexdigest()
@@ -38,7 +58,7 @@ def login(log):
 		'content-type': 'application/json',
 		'X-Bipolar-Signature': signature,
 	}
-	url = MASTER_URL + MASTER_ENDPOINTS['LOGIN']
+	url = MASTER_URL + ENDPOINT_LOG
 
 	f = requests.post(url, json=payload, headers=headers)
 
@@ -54,7 +74,8 @@ def logout(log):
 		"host": l[3],
 		"user": l[8],
 		"ip": l[9],
-		"port": l[11]
+		"port": l[11],
+		"activity": "LOGOUT_SSH"
 	}
 
 	signature  = 'sha1=' + hmac.new(MASTER_SECRET.encode(), json.dumps(payload).encode(), hashlib.sha1).hexdigest()
@@ -63,15 +84,13 @@ def logout(log):
 		'content-type': 'application/json',
 		'X-Bipolar-Signature': signature,
 	}
-	url = MASTER_URL + MASTER_ENDPOINTS['LOGOUT']
+	url = MASTER_URL + ENDPOINT_LOG
 
 	f = requests.post(url, json=payload, headers=headers)
 
 def track():
-	
-	auth_log = CONFIG_VARS['AUTH_LOG']
 
-	f = subprocess.Popen(['tail', '-F', auth_log], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	f = subprocess.Popen(['tail', '-F', AUTH_LOG], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 	
 	while True:
 		line = f.stdout.readline().decode()
